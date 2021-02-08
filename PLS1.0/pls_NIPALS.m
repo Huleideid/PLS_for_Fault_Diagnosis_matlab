c0869c2095cf46d36068488df4b0371342dbce80
Huleideid <leihuneu@gmail.com>
2021-02-08 21:28:53 +0800

    1.添加预测部分 2.修改NIPALS迭代法

--- PLS1.0/pls_NIPALS.m
+++ PLS1.0/pls_NIPALS.m
@@ -1,33 +1,34 @@
 %Hulei
 %20190326
-function [t,p,q,b,w] = pls_NIPALS(X0,Y0, a)
 % PLS (NIPALS algorithm) 
-% a is the number of PLS factors
-[n,m] = size(X0);
-np = size(Y0, 2);
-X = zeros(n,m,a);
-Y = zeros(n,np,a);
-X(:,:,1) = X0;
-Y(:,:,1) = Y0;
-for i = 1:a
-    u(:,i) = Y(:,1,i);
-    itererr = 1;
-    while norm(itererr)>0.000001
-        w(:,i)=X(:,:,i)'*u(:,i)/norm(X(:,:,i)'*u(:,i));
-        w(:,i)= w(:,i)/norm( w(:,i));
-        t(:,i)=X(:,:,i)*w(:,i);
-        q(:,i)=Y(:,:,i)'*t(:,i)/(t(:,i)'*t(:,i));
-        q(:,i)= q(:,i)/norm(q(:,i));
-        u(:,i)=Y(:,:,i)*q(:,i);
-        itererr=t(:,i)-X(:,:,i)*X(:,:,i)'*u(:,i)/norm(X(:,:,i)'*u(:,i));
-
+function [t,p,q,b,w,r] = pls_NIPALS(X0,Y0, a)
+    % a is the number of PLS factors
+    [n,m] = size(X0);
+    X = zeros(n,m,a);
+    Y = zeros(n,size(Y0, 2),a);
+    X(:,:,1) = X0;
+    Y(:,:,1) = Y0;
+    for i = 1:a
+        u(:,i) = Y(:,1,i);
+        itererr = 1;
+        while norm(itererr)>0.0000000001
+            w(:,i)=X(:,:,i)'*u(:,i)/norm(X(:,:,i)'*u(:,i));
+            w(:,i)= w(:,i)/norm( w(:,i));
+            t(:,i)=X(:,:,i)*w(:,i);
+            q(:,i)=Y(:,:,i)'*t(:,i)/(t(:,i)'*t(:,i));
+            q(:,i)= q(:,i)/norm(q(:,i));
+            u(:,i)=Y(:,:,i)*q(:,i);
+            itererr=t(:,i)-X(:,:,i)*X(:,:,i)'*u(:,i)/norm(X(:,:,i)'*u(:,i));
+        end
+        b(:,i) = u(:,i)'*t(:,i)/(t(:,i)'*t(:,i));
+        %得分t正交
+        p(:,i)=X(:,:,i)'*t(:,i)/(t(:,i)'*t(:,i));
+%         t(:,i)=t(:,i)*norm(p(:,i));
+%         p(:,i)=p(:,i)/norm(p(:,i));
+        X(:,:,i+1)=X(:,:,i)-t(:,i)*p(:,i)';
+        Y(:,:,i+1)=Y(:,:,i)-b(:,i)*t(:,i)*q(:,i)'; % y第一层是原始未提取潜变量的y；第二层是提取了第一个潜变量后的残差y，依次类推
     end
-    b(:,i) = u(:,i)'*t(:,i)/(t(:,i)'*t(:,i));
-    %得分t正交
-    p(:,i)=X(:,:,i)'*t(:,i)/(t(:,i)'*t(:,i));
-    p(:,i)=p(:,i)/norm(p(:,i));
-    t(:,i)=t(:,i)*norm(p(:,i));
-    X(:,:,i+1)=X(:,:,i)-t(:,i)*p(:,i)';
-    Y(:,:,i+1)=Y(:,:,i)-b(:,i)*t(:,i)*q(:,i)'; % y第一层是原始未提取潜变量的y；第二层是提取了第一个潜变量后的残差y，依次类推
+    b = diag(b);
+%     r=w*inv(p'*w); 
+    r =0
 end
-b = diag(b);
--- PLS1.0/pls_demo.m
+++ PLS1.0/pls_demo.m
@@ -3,9 +3,9 @@ close all;
 clc; 
 %% 加载数据以及数据预处理
 load ('.\data\d00.mat')
-load ('.\data\d05_te.mat')
+load ('.\data\d01_te.mat')
 d0=d00; 
-d01=d05te; 
+d01=d01te; 
 X_train=d0(:,[1:22,42:52]); 
 Y_train=d0(:,35); 
 X_test=d01(:,[1:22,42:52]); 
@@ -16,19 +16,14 @@ Y_test=d01(:,35);
 X_test=(X_test-repmat(x_mean,size(X_test,1),1))./(repmat(x_v,size(X_test,1),1));
 Y_test=(Y_test-repmat(y_mean,size(X_test,1),1))./(repmat(y_v,size(X_test,1),1));
 %% pls建模
-A = 3;
-[T,P,Q,B,W]=pls_NIPALS(X_train,Y_train,A);
-R=W*inv(P'*W); 
+A = 6;
+[T,P,Q,B,W,R]=pls_NIPALS(X_train,Y_train,A);
+R = W*inv(P'*W);
 n1=size(X_train,1); % 训练数据的样本数
 Ab=inv(T'*T/(n1-1));
-n2=size(X_test,1); 
-for i=1:n2 
-    t_new=R'*X_test(i,:)';%故障数据 
-    tsquare2(i)=t_new'*Ab*t_new; 
-end
-%% 确定控制限 Control Limit
+%% 计算控制限 Control Limit
 alpha=0.95;%显著性水平
-level1=A*(n1*n1-1)/(n1*(n1-A))*finv(alpha,A,(n1-A)); %T2控制限
+T2_lim=A*(n1*n1-1)/(n1*(n1-A))*finv(alpha,A,(n1-A)); %T2控制限
  for i=1:n1 
      xr_old = X_train(i,:)'-P*R'*X_train(i,:)';%正常数据 
      SPE_old(i) = xr_old'*xr_old; 
@@ -36,25 +31,40 @@ level1=A*(n1*n1-1)/(n1*(n1-A))*finv(alpha,A,(n1-A)); %T2控制限
  S1=var(SPE_old); 
  mio1=mean(SPE_old);
  V1=2*mio1^2/S1; 
- level2=S1/(2*mio1)*chi2inv(alpha,V1); %SPE控制限
+ Q_lim=S1/(2*mio1)*chi2inv(alpha,V1); %SPE控制限
+%% 计算监控指标
+n2=size(X_test,1); 
+for i=1:n2 
+    t_new=R'*X_test(i,:)'; 
+    T2(i)=t_new'*Ab*t_new; 
+%     T2(i)=t_new'*Ab*t_new; 
+    xr_new=X_test(i,:)'-P*R'*X_test(i,:)';
+    SPE(i)=xr_new'*xr_new; 
+end
 %% 监控结果可视化
 t = 1:n2;
 figure
 set(gcf,'color','white')
 subplot(2,1,1)
-plot(t,tsquare2,t,ones(n2,1)*level1,'r--'); 
+plot(t,T2,t,ones(n2,1)*T2_lim,'r--'); 
 xlabel('Sample Number'); 
-ylabel('T^2'); % title('PLS-fault data');
- for i=1:n2 
-     xr_new=X_test(i,:)'-P*R'*X_test(i,:)';%故障数据 
-     SPE(i)=xr_new'*xr_new; 
- end
-legend('SPE','95% upper limit','Location','best')
+ylabel('T^2');
+legend('Hotelling''s T^2','95% upper limit','Location','best')
 subplot(2,1,2)
-plot(t,SPE,t,ones(n2,1)*level2,'r--');
+plot(t,SPE,t,ones(n2,1)*Q_lim,'r--');
 xlabel('Sample Number'); 
 ylabel('SPE'); 
-legend('Hotelling''s T^2','95% upper limit','Location','best')
+legend('SPE','95% upper limit','Location','best')
+%% pls预测
+T_pred = X_train * R;
+Y_pred = T_pred *B * Q';
+figure
+set(gcf,'color','white')
+plot(Y_pred,'r')
+hold on 
+plot(Y_train,'b')
+xlabel('样本数')
+ylabel('归一化后的预测情况')
 %% 故障检测率 
 % % T2 fault detection rate 
 % t2_detect=0; 
